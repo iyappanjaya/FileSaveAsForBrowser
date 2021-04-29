96a701cf0369c7494d3e59b46d55409cb04e0a5d
Iyappan <iyappan689@gmail.com>
2021-04-29 11:22:24 +0530

    file save as option window open (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FileUtilService } from './services/file-util.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'downloader';
+  constructor(public fileDownloader: FileUtilService) { }
+  donwload() {
+    this.fileDownloader.getPDF();
+  }
 }
